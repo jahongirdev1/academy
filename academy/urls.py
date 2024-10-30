1a7eb00e3c9a65bd61cb114dbc89ad5923a39f7b
jahongirdev1 <jahonggir617@gmail.com>
2024-10-30 13:07:12 +0500

    change

--- academy/urls.py
+++ academy/urls.py
@@ -1,22 +1,13 @@
 from django.conf.urls.static import static
 from django.contrib import admin
 from django.urls import path
-
-
-
-
-
-from django.conf.urls.static import static
-from django.contrib import admin
-from django.urls import path
-
 from academy import settings
-from main.views import get_user
+from main.views import get_user, courses_list_json
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('get-user/', get_user, name='get-user'),
-
+    path('api/course/', courses_list_json, name='courses_list_json'),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
 
 
--- main/views.py
+++ main/views.py
@@ -1,34 +1,13 @@
-import secrets  # For generating a new token
 from django.http import JsonResponse
 from django.views.decorators.http import require_GET
-from main.models import UserModel
+from main.models import UserModel, CourseModel
+import secrets
 
-@require_GET
-def get_user(request):
-    try:
-        # Retrieve the email, password, and token from query parameters
-        email = request.GET.get('email')
-        password = request.GET.get('password')
-        token = request.GET.get('token')  # Existing token if provided
 
-        if not all([email, password]):
-            return JsonResponse({'error': 'Email and password are required'}, status=400)
-
-        # Fetch the user by email
-        user = UserModel.objects.get(email=email)
-
-        # Check if the provided password matches
-        if user.password != password:  # Replace with a proper password check in production
-            return JsonResponse({'error': 'Invalid password'}, status=400)
-
-        # Prepare user data
-        user_data = {
-            'id': user.id,
-            'first_name': user.first_name,
-            'last_name': user.last_name,
-            'email': user.email,
-            'password': user.password,  # Not recommended to expose password
-            'token': user.token,
+def courses_list_json(request):
+    if request.GET.get('password') == 'jahongirdev1':
+        courses = CourseModel.objects.all()
+        data = {
             'courses': [
                 {
                     'id': course.id,
@@ -43,21 +22,61 @@ def get_user(request):
                             'description': lesson.description
                         } for lesson in course.lessons.all()
                     ]
-                } for course in user.courses.all()
+                } for course in courses
+            ]
+        }
+        return JsonResponse(data, safe=False)
+
+
+
+@require_GET
+def get_user(request):
+    try:
+        email = request.GET.get('email')
+        password = request.GET.get('password')
+        token = request.GET.get('token')
+
+        if not all([email, password]):
+            return JsonResponse({'error': 'Email and password are required'}, status=400)
+
+        user = UserModel.objects.get(email=email)
+
+
+        if user.password != password:
+            return JsonResponse({'error': 'Invalid password'}, status=400)
+
+        user_data = {
+            'id': user.id,
+            'first_name': user.first_name,
+            'last_name': user.last_name,
+            'email': user.email,
+            'password': user.password,
+            'token': user.token,
+            'courses': [
+                {
+                    'id': course.id,
+                    'title': course.title,
+                    'teacher_full_name': course.teacher_full_name,
+                    'teacher_image': course.teacher_image.url if course.teacher_image else None,
+                    'lessons': [
+                        {
+                            'id': lesson.id,
+                            'title': lesson.title,
+                            'video_link': lesson.video_link,
+                            'description': lesson.description
+                        } for lesson in course.lessons.filter(status=0)
+                    ]
+                } for course in user.courses.filter(status=0)
             ]
         }
 
-        # If a token is provided, check if it matches
         if token and user.token != token:
-            # Token is incorrect, generate a new one
-            new_token = secrets.token_hex(16)  # Generate a new 32-character token
+            new_token = secrets.token_hex(16)
             user.token = new_token
-            user.save()  # Save the new token to the database
+            user.save()
 
-            # Update user data with the new token
             user_data['token'] = new_token
 
-        # Return the user data
         return JsonResponse({'user': user_data}, status=200)
 
     except UserModel.DoesNotExist:
